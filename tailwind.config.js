fdf397649da7808034c649beb37bb35affe2c6a1
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-07-02 22:04:17 +0530

    Blog Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "custom-radial": "radial-gradient(150% 150% at 0% 0%, rgba(30, 58, 138, 0.3) 7.75%, rgba(15, 23, 42, 0.3) 79.39%)",
       },
     },
   },
