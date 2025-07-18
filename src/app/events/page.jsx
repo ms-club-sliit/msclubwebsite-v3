3d012f390f41d184038a13227c5d912bedce8004
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2025-07-18 21:09:37 +0530

    do changes realted to hero section and section card showcase

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -3,22 +3,21 @@ import Header from '@/components/Header'
 import React from 'react'
 import EventSection from '@/components/layouts/events/EventSection'
 import Footer from '@/components/Footer'
+import BackgroundContainer from '@/components/BackgroundContainer'
+import Hero from '@/components/Hero'
 
 const page = () => {
   return (
     <div className="relative min-h-screen bg-gray-900 text-white">
 
-      <div className='px-6 md:px-12'>
-        <Header />
-
-        {/* hero section */}
-        <EventHeroSection />
-
-        {/* show event details */}
-        <EventSection />
-      </div>
-
-      {/* footer section */}
+      <Header />
+      <Hero
+        title='Explore Events'
+        paragraph='Explore all the events organized by our students and to boost your skills and expand your network.'
+        button1Text='More Info On Facebook'
+        button2Text='Subscribe on Youtube'
+      />
+      <EventSection />
       <Footer />
         
     </div>
