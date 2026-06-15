db29dc575fc2a8d849d86e1c33896b9bd5e1e806
Thamidu-Nadun <nadunrz101@gmail.com>
2026-06-15 21:21:14 +0530

    tesing husky

---
+++ src/utils/temp.js
@@ -0,0 +1,16 @@
+// This file is for testing husky and lint-staged. It will be deleted later.
+
+function add(a, b) {
+  return a + b;
+}
+
+const user = {
+  name: "Alice",
+  age: 30,
+};
+
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+greet(user.name);
